2fbb92e9a243650305bf5dadf33a1759db7fdfa8
Cameron McLaren <cameron_mclaren@outlook.com>
2025-07-20 11:16:59 +0100

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -4,7 +4,7 @@ import random
 
 window = tk.Tk()
 window.title("Password Generator")
-window.geometry("350x150")
+window.geometry("350x175")
 
 password_display = tk.Entry(window, font=("Arial", 20), justify="center")
 password_display.pack(pady=10, ipadx=10, ipady=10)
@@ -13,10 +13,22 @@ def generate_password():
     length = 18
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
+
     password_display.delete(0, tk.END)
     password_display.insert(0, password)
 
-generate_button = tk.Button(text="Generate Password", font=("Arial", 20), command=generate_password)
-generate_button.pack(pady=10, padx=20)
+genarate_button = tk.Button(text="Generate Password", font="Arial, 20",
+command=generate_password)
+genarate_button.pack(pady=5, padx=20)
+
+def copy_to_clipboard():
+    password= password_display.get()
+    window.clipboard_clear()
+    window.clipboard_append(password)
+    
+
+copy_button = tk.Button(text="Copy", font="Arial, 10",
+command=copy_to_clipboard)
+copy_button.pack(pady=2)
 
 window.mainloop()
